ac6c0bef193c65b728a0f9dba43aeb084804a00b
sara.kuenzi <48669509+skuenzi@users.noreply.github.com>
2022-03-14 01:17:34 -0600

    finished logic

--- index.js
+++ index.js
@@ -27,58 +27,106 @@ custTipInput.addEventListener('input', getValue)
 // get tip percentage
 
 let tipPercentage
+let percentageBtns
 
 document.addEventListener('DOMContentLoaded', () => { // wait for DOM content to load, then add event listener to each button
-    const percentageBtns = document.getElementsByClassName('perc-btn')
+    percentageBtns = document.getElementsByClassName('perc-btn')
 
     for(let i = 0; i < percentageBtns.length; i++) {
         percentageBtns[i].addEventListener('click', getPercentage)
-        percentageBtns[i].addEventListener('click', getTotalTip)
+        percentageBtns[i].addEventListener('blur', getTotals)
 
     }
 
 })
-custTipInput.addEventListener('click', getPercentage)
+custTipInput.addEventListener('input', handleCustTip)
 
 function getPercentage (e) {
-    if (e.target === custTip) {
-        tipPercentage = parseFloat(custTip)
-    }
     tipPercentage = parseInt(e.target.value, 10)
-    console.log(typeof tipPercentage)
+    for(let i = 0; i < percentageBtns.length; i++) {
+        if (percentageBtns[i].value == tipPercentage) {
+            percentageBtns[i].classList.add('selected')
+        } else {
+            percentageBtns[i].classList.remove('selected')
+        }
+    }
+}
+
+function handleCustTip (e) {
+    if (e.target === custTipInput) {
+        tipPercentage = e.target.value
+    }
 }
 
 
-// get total tip amount 
 
-
-let totalTip
-
-function getTotalTip () {
-        totalTip = bill * (tipPercentage/100)
-        console.log(totalTip)
-}
-
-// get tip /person
+// get tip and total /person
 
 let tipPerPerson = document.getElementsByClassName('end-calc-tip')[0]
-
-peopleInput.addEventListener('input', getTipPerPerson)
-
-
-
-function getTipPerPerson () {
-    tipPerPerson.innerHTML = totalTip / people
-    console.log(tipPerPerson.innerHTML)
-}
-
-
-// get total per person
-
 let totalPerPerson = document.getElementsByClassName('total')[0]
+let totalTip
 
-function getTotalPerPerson () {
-    totalPerPerson.innerHTML = (bill + totalTip) / people
+peopleInput.addEventListener('change', getTotals)
+custTipInput.addEventListener('blur', getTotals)
+
+
+
+function getTotals (e) {
+    totalTip = bill * (tipPercentage/100)
+
+    if (e.target.classList.contains('perc') && people && bill) {
+        tipPerPerson.innerHTML = formatter.format((totalTip / people))
+        totalPerPerson.innerHTML = formatter.format((bill + totalTip) / people)
+    }  
+    if (tipPercentage && bill && people) {
+        tipPerPerson.innerHTML = formatter.format((totalTip / people))
+        totalPerPerson.innerHTML = formatter.format((bill + totalTip) / people)
+    }
 }
 
-peopleInput.addEventListener('input', getTotalPerPerson)
+let formatter = new Intl.NumberFormat(undefined, {
+    style: 'currency',
+    currency: 'USD'
+})
+
+
+// reset 
+
+const resetBtn = document.getElementsByClassName('reset-btn')[0]
+
+resetBtn.addEventListener('click', reset)
+
+function reset () {
+    billInput.value = 0
+    peopleInput.value = 0 
+    for(let i = 0; i < percentageBtns.length; i++) {
+        percentageBtns[i].classList.remove('selected')
+    }
+    custTipInput.value = 'Custom'
+    tipPerPerson.innerHTML = '--'
+    totalPerPerson.innerHTML = '--'
+}
+
+// check for empty fields
+
+
+function checkFields (e) {
+    if (e.target.value < 1) {
+        e.target.classList.add('empty')
+        if (e.target === billInput) {
+            document.getElementsByClassName('error')[0].classList.remove('hidden')
+        } else if (e.target === peopleInput) {
+            document.getElementsByClassName('error')[1].classList.remove('hidden')
+        }
+    } else {
+        e.target.classList.remove('empty')
+        if (e.target === billInput) {
+            document.getElementsByClassName('error')[0].classList.add('hidden')
+        } else if (e.target === peopleInput) {
+            document.getElementsByClassName('error')[1].classList.add('hidden')
+        }
+    }
+}
+
+billInput.addEventListener('blur', checkFields)
+peopleInput.addEventListener('blur', checkFields)
